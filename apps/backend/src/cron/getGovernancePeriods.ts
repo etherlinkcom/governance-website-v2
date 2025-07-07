637f37bdc28cd3b94685663ae52d32d60dfd10db
Devon <devnathan94@gmail.com>
2025-07-07 16:01:30 +0400

    chore: plan

--- apps/backend/src/cron/getGovernancePeriods.ts
+++ apps/backend/src/cron/getGovernancePeriods.ts
@@ -1,1 +1,22 @@
-console.log('This file is used to get governance periods from the database.');
+
+
+const contracts = [
+    {
+        type: 'fast',
+        address: 'KT1FPG4NApqTJjwvmhWvqA14m5PJxu9qgpBK',
+    },
+    {
+        type: 'slow',
+        address: 'KT1FPG4NApqTJjwvmhWvqA14m5PJxu9qgpBK',
+    },
+]
+
+
+function checkPromotion() {
+    const winning_candidate = ''; // SH.value.voting_context.period.proposal.winning_candidate
+    const max_upvotes_voting_power = 0; // SH.value.voting_context.period.proposal.max_upvotes_voting_power
+    // check if period has ended
+    // check if (max_upvotes_voting_power / total_voting_power) * 100 > promotion_quorum
+    // insert proposal into promotions table
+
+}
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,12 +1,17 @@
 export type NetworkType = 'mainnet' | 'testnet';
 export type GovernanceType = 'slow' | 'fast' | 'sequencer';
+
+// storageHistory = https://api.tzkt.io/v1/contracts/KT1FPG4NApqTJjwvmhWvqA14m5PJxu9qgpBK/storage/history?limit=1000
+// operations = /v1/operations/transactions/{hash})
+
 export type Period = {
   id: number;
-  governance_type: GovernanceType;
-  level_start: number;
-  level_end: number;
-  date_start: Date;
-  date_end: Date;
+  governance_type: GovernanceType; // Manual entrance
+  level_start: number; // ((currentBlock - contractConfig.level_start) / contractConfig.period_length) - currentBlock
+  level_end: number; // level_start + contractConfig.period_length
+  date_start: Date; // https://api.tzkt.io/v1/blocks/{level_start}/timestamp
+  date_end: Date; // https://api.tzkt.io/v1/blocks/{level_end}/timestamp
+  /** Contract Configuration (Could be own table) */
   started_at_level: number;
   period_length: number;
   adoption_period_sec: number;
@@ -16,26 +21,36 @@ export type Period = {
   proposal_quorum: number;
   promotion_quorum: number;
   promotion_supermajority: number;
+
   // Relations
-  proposals: number[];
-  promotions: number[];
+  proposals?: string[]; // Calculated
+  promotions?: string; // Calculated
 };
 
 export type ProposalStatus = 'pending' | 'promoted' | 'rejected';
 
 export type Proposal = {
   id: number;
-  key: string; // PayloadKey
-  proposer: string; // Address
-  period_id: number; // Foreign key to Period
-  governance_type: GovernanceType;
-  status: ProposalStatus;
+  key: string; // SH.operation.param.value
+  proposer: string; // operations.sender.address
+  period_id: number; // SH.value.period_index
+  governance_type: GovernanceType; // ContractConfig.governance_type
   // Indexes: key, proposer, period_id, governance_type
 };
 
+/**
+
+promotion_quorum
+total_voting_power
+max_upvotes_voting_power
+
+
+max_upvotes_voting_power / total_voting_power > promotion_quorum ?
+Check if a proposal.period_id.level_start and level_end
+ */
 export type Promotion = {
   id: number;
-  proposal_id: number; // Foreign key to Proposal
+  proposal_id: string; // Foreign key to Proposal
   period_id: number;   // Foreign key to Period
   governance_type: GovernanceType;
   // Indexes: proposal_id, period_id, governance_type
@@ -43,19 +58,21 @@ export type Promotion = {
 
 export type Upvote = {
   id: number;
-  proposal_id: number; // Foreign key to Proposal
-  baker: string;
-  voting_power: number;
+  proposal_id: string; // SH.operation.param.value
+  baker: string; // operations.sender.address
+  alias?: string; // operations.sender.alias
+  voting_power: number; // Calculated using Adrians function
   time: Date;
   // Indexes: proposal_id, baker
 };
 
 export type Vote = {
   id: number;
-  promotion_id: number; // Foreign key to Promotion
-  baker: string;
-  voting_power: number;
-  vote: 'yea' | 'nay' | 'pass';
+  promotion_id: number; // SH.value.voting_context.period.promotion.winning_candidate
+  baker: string; // operations.sender.address
+  alias?: string; // operations.sender.alias
+  voting_power: number; // Calculated using Adrians function
+  vote: 'yea' | 'nay' | 'pass'; // SH.operation.parameter.value
   time: Date;
   // Indexes: promotion_id, baker
 };
