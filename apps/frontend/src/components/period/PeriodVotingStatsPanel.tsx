4779ff55340db4054e7abf4afb7cc84cc4f1674d
Devon <devnathan94@gmail.com>
2025-07-31 14:41:01 +0400

    feat: promotion results on periodCard

--- apps/frontend/src/components/period/PeriodVotingStatsPanel.tsx
+++ apps/frontend/src/components/period/PeriodVotingStatsPanel.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import {
   PromotionVotingStats,
   ProposalVotingStats,
@@ -30,31 +30,62 @@ export const PeriodVotingStatsPanel = ({
   isLoading,
   period,
 }: PeriodVotingStatsPanelProps) => {
+  const theme = useTheme();
+
   if (hasPromotion) {
+    let totalVotingPower = 0;
+    if (promotions && promotions[0]) {
+      totalVotingPower = promotions[0].yea_voting_power +
+        promotions[0].nay_voting_power +
+        promotions[0].pass_voting_power ||
+        0;
+    }
+
     return (
       <Box
         sx={{
           position: "absolute",
           right: 24,
           display: { xs: "none", md: "block" },
+          width: "255px",
         }}
       >
         {isLoading ? (
           <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
-            <ComponentLoading width="240px" borderRadius={2} sx={{ mt: 1 }} />
-            <ComponentLoading width="240px" borderRadius={2} />
+            <ComponentLoading width="255px" borderRadius={2} sx={{ mt: 1 }} />
+            <ComponentLoading width="255px" borderRadius={2} />
+            <ComponentLoading width="255px" borderRadius={2} />
           </Box>
         ) : promotions && promotions[0] ? (
-          <PromotionVotingStats
-            yeaVotingPower={promotions[0].yea_voting_power || 0}
-            nayVotingPower={promotions[0].nay_voting_power || 0}
-            passVotingPower={promotions[0].pass_voting_power || 0}
-            totalVotingPower={promotions[0].total_voting_power || 0}
-            contractQuorum={contractAndConfig?.promotion_quorum || 0}
-            contractSupermajority={
-              contractAndConfig?.promotion_supermajority || 0
-            }
-          />
+          <>
+            <PromotionVotingStats
+              yeaVotingPower={promotions[0].yea_voting_power || 0}
+              nayVotingPower={promotions[0].nay_voting_power || 0}
+              passVotingPower={promotions[0].pass_voting_power || 0}
+              totalVotingPower={promotions[0].total_voting_power || 0}
+              contractQuorum={contractAndConfig?.promotion_quorum || 0}
+              contractSupermajority={
+                contractAndConfig?.promotion_supermajority || 0
+              }
+            />
+            <Box sx={{ display: "flex", gap: 1 }}>
+              <Box>
+                <Typography sx={{ color: `${theme.palette.success.main} !important` }}>
+                  Yea: {((promotions[0].yea_voting_power / totalVotingPower) * 100).toFixed(1)}%
+                </Typography>
+              </Box>
+              <Box>
+                <Typography sx={{ color: `${theme.palette.error.main} !important` }}>
+                  Nay: {((promotions[0].nay_voting_power / promotions[0].total_voting_power) * 100).toFixed(1)}%
+                </Typography>
+              </Box>
+              <Box>
+                <Typography sx={{ color: `${theme.palette.warning.main} !important` }}>
+                  Pass: {((promotions[0].pass_voting_power / totalVotingPower) * 100).toFixed(1)}%
+                </Typography>
+              </Box>
+            </Box>
+          </>
         ) : null}
       </Box>
     );
@@ -67,11 +98,11 @@ export const PeriodVotingStatsPanel = ({
           position: "absolute",
           right: 24,
           display: { xs: "none", md: "block" },
-          width: "240px",
+          width: "255px",
         }}
       >
         {isLoading ? (
-          <ComponentLoading width="240px" sx={{ mt: 2 }} />
+          <ComponentLoading width="255px" sx={{ mt: 2 }} />
         ) : proposals && period && proposals[0] ? (
           <ProposalVotingStats
             proposals={proposals}
