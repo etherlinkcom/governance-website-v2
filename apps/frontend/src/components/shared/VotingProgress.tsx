8ff91e20740064f686b832f04fd9281e9cbeee95
Devon <devnathan94@gmail.com>
2025-07-31 14:55:38 +0400

    chore: progress bar color

--- apps/frontend/src/components/shared/VotingProgress.tsx
+++ apps/frontend/src/components/shared/VotingProgress.tsx
@@ -1,12 +1,19 @@
-import { Box, darken, LinearProgress, Tooltip, Typography, useTheme } from '@mui/material';
-import { InfoIcon } from './InfoIcon';
+import {
+  Box,
+  darken,
+  LinearProgress,
+  Tooltip,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { InfoIcon } from "./InfoIcon";
 
 interface VotingProgressProps {
   label: string;
   value: string;
   required: number;
   progress: number;
-  variant?: 'subtitle1' | 'body1';
+  variant?: "subtitle1" | "body1";
 }
 
 export const VotingProgress = ({
@@ -14,41 +21,64 @@ export const VotingProgress = ({
   value,
   required,
   progress,
-  variant = 'subtitle1'
+  variant = "subtitle1",
 }: VotingProgressProps) => {
-    const theme = useTheme();
+  const theme = useTheme();
   const baseColor = theme.palette.primary.main;
   const hoverColor = darken(baseColor, 0.5);
+  const progressBarColor = Number(value) >= required ?
+      theme.palette.success.main :
+      theme.palette.error.main;
+  const progressBarBackgroundColor = darken(progressBarColor, 0.5);
 
   return (
     <Box
       sx={{
-        textAlign: 'right',
-        '& .info-icon': {
+        textAlign: "right",
+        "& .info-icon": {
           color: baseColor,
         },
-        '&:hover .info-icon': {
+        "&:hover .info-icon": {
           color: hoverColor,
         },
       }}
     >
-      <Tooltip title={`Current Voting Power: ${value}% / ${required}%`} placement="top" arrow={true}>
-       <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', gap: 1 }}>
-        <Typography variant={variant}>{label}</Typography>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-          <Typography variant={variant}>{value}% / {required}%</Typography>
-          <InfoIcon className="info-icon" color={baseColor} />
+      <Tooltip
+        title={`Current Voting Power: ${value}% / ${required}%`}
+        placement="top"
+        arrow={true}
+      >
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              gap: 1,
+            }}
+          >
+            <Typography variant={variant}>{label}</Typography>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <Typography variant={variant}>
+                {value}% / {required}%
+              </Typography>
+              <InfoIcon className="info-icon" color={baseColor} />
+            </Box>
+          </Box>
+          <LinearProgress
+            variant="determinate"
+            value={progress}
+            sx={{
+              width: "100%",
+              mb: 1,
+              backgroundColor: progressBarBackgroundColor,
+              '& .MuiLinearProgress-bar': {
+                backgroundColor: progressBarColor,
+              },
+            }}
+          />
         </Box>
-      </Box>
       </Tooltip>
-      <LinearProgress
-        variant="determinate"
-        value={progress}
-        sx={{
-          width: '100%',
-          mb: 1
-        }}
-      />
     </Box>
   );
 };
