ef599f2b3ff88fc57c1e0ecacd597e3959c6ab93
Devon <devnathan94@gmail.com>
2025-08-15 13:37:17 +0400

    chore: add posthog

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -6,13 +6,27 @@ import { Toaster } from 'react-hot-toast';
 import { theme } from '@/theme';
 import { Layout } from "@/components/layouts/Layout";
 import { toastConfig } from '@/theme/toastConfig';
+import { PostHogProvider } from 'posthog-js/react'
+import posthog from 'posthog-js'
+import { useEffect } from 'react'
+
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+      defaults: '2025-05-24',
+    })
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Layout>
-        <Component {...pageProps} />
+        <PostHogProvider client={posthog}>
+          <Component {...pageProps} />
+        </PostHogProvider>
       </Layout>
       <Toaster {...toastConfig} />
     </ThemeProvider>
