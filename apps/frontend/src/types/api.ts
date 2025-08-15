dd1081ccb636aa6d01cce990ab084c9ce5668ee8
Devon <devnathan94@gmail.com>
2025-08-15 17:42:32 +0400

    fix: total voting power query

--- apps/frontend/src/types/api.ts
+++ apps/frontend/src/types/api.ts
@@ -37,7 +37,7 @@ export interface FrontendPeriod {
     contract: string;
     governance: string;
     contract_voting_index: number;
-    total_voting_power: number;
+    totalVotingPower: number;
     proposals?: Proposal[];
     promotion?: Promotion;
 }
