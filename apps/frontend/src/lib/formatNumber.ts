c573651ad272c62cf5a13fb8a79202c585635825
Devon <devnathan94@gmail.com>
2025-12-12 15:41:22 +0400

    chore: update nextjs

--- apps/frontend/src/lib/formatNumber.ts
+++ apps/frontend/src/lib/formatNumber.ts
@@ -1,5 +1,7 @@
+import { BigNumber } from 'bignumber.js';
+
 export const formatNumber = (num: number | BigNumber): string => {
-    const displayInTez = process.env.DISPLAY_IN_TEZ === 'true';
+    const displayInTez = process.env.NEXT_PUBLIC_DISPLAY_IN_TEZ === 'true';
     const value = Number(displayInTez ? (num instanceof BigNumber ? num.div(1_000_000) : num / 1_000_000) : num);
     return new Intl.NumberFormat(undefined, {
       notation: 'compact',
